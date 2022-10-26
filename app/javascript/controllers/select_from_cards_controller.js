c5b7aecc255c238bc56e9665e6a7d63b0c77890b
sofia ngapani <withsofina@gmail.com>
2022-10-26 22:41:10 +0800

    pulled API from oct to nov for demo int

--- app/javascript/controllers/select_from_cards_controller.js
+++ app/javascript/controllers/select_from_cards_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
   static targets = ["card"];
 
   connect() {
-    console.log("stimulus connected 22");
+    console.log("stimulus connected 60");
     console.log(this.cardTarget);
   }
 
@@ -13,18 +13,18 @@ export default class extends Controller {
     event.preventDefault();
     console.log(this.cardTargets);
     const flights = this.cardTarget.dataset.amount;
+
     this.cardTargets.forEach((card) => {
       event.currentTarget.dataset.value !== card.dataset.value &&
         card.firstElementChild.classList.remove("card-focus");
     });
+
     this.cardTargets[
       event.currentTarget.dataset.value
     ].firstElementChild.classList.toggle("card-focus");
 
     console.log(`${event.currentTarget.dataset.value} is selected`);
-    console.log(`${event.currentTarget}`);
-    // console.log(flights)0
-    //console.log(flights.dataset.amount);
+
   }
 
   select(event) {
