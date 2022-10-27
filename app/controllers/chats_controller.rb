24e00e7a000457e41da292ecc5583f56b4791556
sofia ngapani <withsofina@gmail.com>
2022-10-27 14:40:16 +0800

    chat-demo

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -3,14 +3,11 @@ class ChatsController < ApplicationController
     @chats = Chat.all
   end
 
-  def create
-  end
-
   def show
     @chat = Chat.find(params[:id])
+
     @message = Message.new
     if current_user.role == "admin"
-    else
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,12 +18,12 @@ Rails.application.routes.draw do
     get "trip-generate/", to: "pages#generate", as: :generate_page
   end
 
-  resources :chats, only: %i[index create show] do
+  resources :chats, only: %i[index show] do
     resources :messages, only: :create
   end
 
   #get "pages/", to: "pages#generate", as: :generate_page
-  
+
   get "dashboard", to: "pages#dashboard", as: :dashboard_page do
     get "trips/show", to: "trips#show"
   end
